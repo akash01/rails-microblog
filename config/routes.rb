8e41e88db3a3a56ba6f81abdff093ca66caa9013
mukesh <pace.akash@gmail.com>
2014-02-01 03:13:29 +0200

    finished static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Microblog::Application.routes.draw do
   get "static_pages/home"
   get "static_pages/help"
+  get "static_pages/about"
+  
   resources :microposts
 
   root 'users#index'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -39,4 +39,7 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
+
+  # Make the Capybara DSL available in all integration tests
+  config.include Capybara::DSL
 end
